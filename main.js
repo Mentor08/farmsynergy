809ed69d467d44d84f4848582923dd076eb72f5a
mentor08 <ekundayomayowa83@gmail.com>
2024-04-14 21:24:24 +0100

    farmsynergy

--- main.js
+++ main.js
@@ -14,16 +14,13 @@ function hidePreloader() {
 }
 
 // Timeout duration in milliseconds (e.g., 5 seconds)
-//var timeoutDuration = 5000;
+var timeoutDuration = 5000;
 
 // Set timeout to hide the preloader after a certain duration
-//var timeout = setTimeout(hidePreloader, timeoutDuration);
+var timeout = setTimeout(hidePreloader, timeoutDuration);
 
 // Optional: If you want to clear the timeout if the content loads before the timeout
 window.addEventListener("load", function () {
-  //clearTimeout(timeout);
-  //hidePreloader();
-  var timeout = setTimeout(() => {
-    document.querySelector("body").classList.add("loaded");
-  }, 3000);
+  clearTimeout(timeout);
+  hidePreloader();
 });
